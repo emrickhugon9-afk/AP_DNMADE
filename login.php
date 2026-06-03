c8fb5e0f58e06507be32f3f9066dea7f9529dee8
stéphanie tep <smeyytp119@hotmail.com>
2026-06-03 08:48:48 +0200

    add newpages

--- login.php
+++ login.php
@@ -1,26 +1,28 @@
-<?php if (isset($erreur)) { ?>
-    <p style="color:red;"><?php echo $erreur; ?></p>
-<?php } ?>
 <?php
+$temps_inactivite = 1440; 
+
+session_set_cookie_params($temps_inactivite);
+
 session_start();
 
 if (isset($_POST['submit'])) {
 
     if (empty($_POST['login']) || empty($_POST['password'])) {
         $erreur = "Champs obligatoires !";
-    } 
+        $_POST['login'] = '';
+        $_POST['password'] = '';
+    }
     else {
         $login = $_POST['login'];
         $password = $_POST['password'];
-        $m = '/^\S*(?=\S{8,})(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$/';
             
             if(isset($_POST['password'])){
-                if(preg_match($m, $_POST['password'])){
+                if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/', $_POST['password'])) {
                     echo 'Le mot de passe choisi convient';
-                }else{
+                } else {
                     echo 'Le mot de passe choisi ne répond pas aux critères';
                 }
-            }
+            } 
         if ($login === "admin" && $password === "1234") {
             $_SESSION["user"] = $login;
             header("Location: index.html"); 
