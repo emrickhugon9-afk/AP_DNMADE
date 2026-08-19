4205d8fb7121a9fbfcd4f2b929df8961a9221901
nathanchtz <nathanchtz@gmail.com>
2026-08-19 11:56:03 +0200

    Titre & Logo

--- CRUD/user_add.php
+++ CRUD/user_add.php
@@ -27,7 +27,8 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Success Page</title>
+    <title>DNMADE - Ajout</title>
+    <link rel="icon" type="image/x-icon" href="/img/logo_costumerie.png">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
--- CRUD/user_delete.php
+++ CRUD/user_delete.php
@@ -25,7 +25,8 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Success Page</title>
+    <title>DNMADE - Suppression</title>
+    <link rel="icon" type="image/x-icon" href="/img/logo_costumerie.png">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
